8b9b664dceeaf1960cff510a78e3223e32a224ff
jun <jyuan2@pace.edu>
2020-11-18 14:13:41 -0500

    rename

---
+++ project3/src/Utils/BoundBuffer.java
@@ -0,0 +1,8 @@
+package Utils;
+
+public class BoundBuffer <T> {
+    //Please note the biggest difference between this BoundBuffer
+    //and the one we demoed in class is <T>
+   // implement member functions: deposit() and fetch()
+
+}
