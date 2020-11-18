f572b0e925e6d021cb97852c3c55a828c93ea96c
jun <jyuan2@pace.edu>
2020-11-18 14:29:08 -0500

    rename

--- project3/src/PartitionTable.java
+++ project3/src/PartitionTable.java
@@ -1,12 +1,12 @@
-import Utils.UnboundBuffer;
+import Utils.BoundBuffer;
 import java.util.HashMap;
 
 import Utils.*;
 public class PartitionTable {
 	//TODO: your codde here
 	//Notes:
-	// (1) each partition works like an unbound buffer between
-	// mappers and a reducer.
+	// (1) each partition works like an bounded buffer between
+	// mappers and a reducer. (you can assume size = 10 or 50)
 	// (2) if reducer_i wants to fetch a KV pair it can
 	// only fetches from partition_i
 	// (3) reducer_i also has to maintain a kv store which maps
